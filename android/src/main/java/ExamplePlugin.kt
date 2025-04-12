2058594c4da7c357206e03d5eebd9cad2e660f0a
Joseph Birks <joseph@redfernelectronics.co.uk>
2025-04-12 18:17:41 +0100

    Added enumerate code on Android

--- android/src/main/java/ExamplePlugin.kt
+++ android/src/main/java/ExamplePlugin.kt
@@ -5,8 +5,15 @@ import app.tauri.annotation.Command
 import app.tauri.annotation.InvokeArg
 import app.tauri.annotation.TauriPlugin
 import app.tauri.plugin.JSObject
+import app.tauri.plugin.JSArray
 import app.tauri.plugin.Plugin
 import app.tauri.plugin.Invoke
+import android.hardware.usb.UsbConstants
+import android.hardware.usb.UsbEndpoint
+import android.hardware.usb.UsbInterface
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
+import android.content.Context
 
 @InvokeArg
 class PingArgs {
@@ -23,6 +30,30 @@ class ExamplePlugin(private val activity: Activity): Plugin(activity) {
 
         val ret = JSObject()
         ret.put("value", implementation.pong(args.value ?: "default value :("))
+        invoke.resolve(ret)
+    }
+
+    @Command
+    fun enumerate(invoke: Invoke) {
+        val ret = JSObject()
+        val devices = JSArray()
+
+        val usbManager = activity.getSystemService(Context.USB_SERVICE) as UsbManager
+        val deviceList = usbManager.deviceList
+
+        for (dev in deviceList.values) {
+            val device = JSObject()
+            device.put("path", dev.deviceName)
+            device.put("vendorId", dev.vendorId)
+            device.put("productId", dev.productId)
+            device.put("serialNumber", dev.serialNumber)
+            device.put("releaseNumber", 0)
+            device.put("manufacturerString", dev.manufacturerName)
+            device.put("productString", dev.productName)
+            devices.put(device)
+        }
+
+        ret.put("devices", devices)
         invoke.resolve(ret)
     }
 }
