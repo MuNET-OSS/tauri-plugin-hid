3b0cb598d662e4d82ce4b10f41eea9ba8ef8cd72
Joseph Birks <joseph@redfernelectronics.co.uk>
2025-04-13 16:03:40 +0100

    Added error mapping to enumerate

--- src/mobile.rs
+++ src/mobile.rs
@@ -1,7 +1,7 @@
 use serde::de::DeserializeOwned;
 use tauri::{
-  plugin::{PluginApi, PluginHandle},
-  AppHandle, Runtime,
+    plugin::{PluginApi, PluginHandle},
+    AppHandle, Runtime,
 };
 
 use crate::models::*;
@@ -11,45 +11,48 @@ tauri::ios_plugin_binding!(init_plugin_hid);
 
 // initializes the Kotlin or Swift plugin classes
 pub fn init<R: Runtime, C: DeserializeOwned>(
-  _app: &AppHandle<R>,
-  api: PluginApi<R, C>,
+    _app: &AppHandle<R>,
+    api: PluginApi<R, C>,
 ) -> crate::Result<Hid<R>> {
-  #[cfg(target_os = "android")]
-  let handle = api.register_android_plugin("uk.redfern.tauri.plugin.hid", "ExamplePlugin")?;
-  #[cfg(target_os = "ios")]
-  let handle = api.register_ios_plugin(init_plugin_hid)?;
-  Ok(Hid(handle))
+    #[cfg(target_os = "android")]
+    let handle = api.register_android_plugin("uk.redfern.tauri.plugin.hid", "ExamplePlugin")?;
+    #[cfg(target_os = "ios")]
+    let handle = api.register_ios_plugin(init_plugin_hid)?;
+    Ok(Hid(handle))
 }
 
 /// Access to the hid APIs.
 pub struct Hid<R: Runtime>(PluginHandle<R>);
 
 impl<R: Runtime> Hid<R> {
-  // pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
-  //   self
-  //     .0
-  //     .run_mobile_plugin("ping", payload)
-  //     .map_err(Into::into)
-  // }
+    // pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+    //   self
+    //     .0
+    //     .run_mobile_plugin("ping", payload)
+    //     .map_err(Into::into)
+    // }
 
-  pub fn enumerate(&self) -> crate::Result<Vec<HidDeviceInfo>> {
-    let response: EnumerateResponse = self.0.run_mobile_plugin("enumerate", ())?;
-    Ok(response.devices)
-  }
-  
-  pub fn open(&self, path: &str) -> crate::Result<()> {
-    Err(crate::Error::HidDeviceNotFound)
-  }
+    pub fn enumerate(&self) -> crate::Result<Vec<HidDeviceInfo>> {
+        let response: EnumerateResponse = self
+            .0
+            .run_mobile_plugin("enumerate", ())
+            .map_err(Into::into)?;
+        Ok(response.devices)
+    }
 
-  pub fn close(&self, path: &str) -> crate::Result<()> {
-    Err(crate::Error::HidDeviceNotFound)
-  }
+    pub fn open(&self, path: &str) -> crate::Result<()> {
+        Err(crate::Error::HidDeviceNotFound)
+    }
 
-  pub fn write(&self, path: &str, data: &[u8]) -> crate::Result<()> {
-    Err(crate::Error::HidDeviceNotFound)
-  }
+    pub fn close(&self, path: &str) -> crate::Result<()> {
+        Err(crate::Error::HidDeviceNotFound)
+    }
 
-  pub fn read(&self, path: &str, timeout: i32) -> crate::Result<Vec<u8>> {
-    Err(crate::Error::HidDeviceNotFound)
-  }
+    pub fn write(&self, path: &str, data: &[u8]) -> crate::Result<()> {
+        Err(crate::Error::HidDeviceNotFound)
+    }
+
+    pub fn read(&self, path: &str, timeout: i32) -> crate::Result<Vec<u8>> {
+        Err(crate::Error::HidDeviceNotFound)
+    }
 }
